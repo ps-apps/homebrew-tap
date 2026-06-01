b2d4ecbdd9663e4b9db7f917e42b86b1d00d5460
Pankaj Singh <theiterator@gmail.com>
2026-06-01 17:00:50 +0530

    cursorshot 0.4.2

--- Casks/cursorshot.rb
+++ Casks/cursorshot.rb
@@ -1,6 +1,6 @@
 cask "cursorshot" do
-  version "0.4.1"
-  sha256 "160f8e6abfe88ae1d6aaa79e42e638f0c1c00d06ac64fdd68842aa8e2285b042"
+  version "0.4.2"
+  sha256 "3d3886e1410fa63158aef80643e9bd49af87a2b7643f63b147fcf24f79121e27"
 
   url "https://github.com/ps-apps/CursorShot/releases/download/v#{version}/CursorShot.dmg",
       verified: "github.com/ps-apps/CursorShot/"
